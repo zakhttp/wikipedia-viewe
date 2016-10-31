018479efe2d5ca335030482be22b34e01c700628
zakhttp <zakhttp@localhost.localdomain>
2016-10-31 16:41:36 +0400

    bind request to submit events

--- js/app.js
+++ js/app.js
@@ -1,30 +1,69 @@
 //$.getJSON("https://en.wikipedia.org/w/api.php?action=query&generator=search&gsrnamespace=0&gsrsearch=te&gsrlimit=10&prop=info|extracts&inprop=url&format=json&callback=?", processResult);
 
 $(document).ready(function() {
-    var url = 'https://en.wikipedia.org/w/api.php';
-    var data = {
-        action: 'query',
-        generator: 'search',
-        gsrnamespace: 0,
-        gsrsearch: 'tesla',
-        gsrlimit: 10,
-        prop: 'info|extracts',
-        inprop: 'url',
-        format: 'json',
-        origin: '*'
-    };
+  var searchForm = $('#search-form');
+  var searchButton = $('#search-btn');
+  var randomButton = $('#random-btn');
+  var results = $('.results-container');
 
+
+  var url = 'https://en.wikipedia.org/w/api.php';
+  var urlData = {
+    action: 'query',
+    generator: 'search',
+    gsrnamespace: 0,
+    gsrlimit: 10,
+    prop: 'extracts',
+    titles: 'Main page',
+    exsentences: 1,
+    exlimit: 10,
+    exintro: 1,
+    explaintext: 1,
+    format: 'json',
+    origin: '*'
+  };
+  // /w/api.php?action=query&format=json&origin=*&prop=extracts&titles=Main+Page&generator=search&exsentences=2&exlimit=10&exintro=1&explaintext=1&exsectionformat=raw&gsrsearch=tesla
+    // https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=tesla&callback=angular.callbacks._1
+
+  function success(json) {
+    console.log(json);
+  }
+
+  function fail() {
+    console.log('fail');
+  }
+
+  function fetchData() {
+    urlData.gsrsearch = $('#search-input').val();
     $.ajax({
-            dataType: 'json',
-            url: url,
-            data: data,
-        }).done(success)
-        .fail(fail);
+        method: 'GET',
+        datatype: 'json',
+        url: url,
+        data: urlData
+      })
+      .done(success)
+      .fail(fail);
+  }
 
-    function success(json) {
-        console.log(json);
-    }
-    function fail() {
-        console.log('fail');
-    }
+
+  function handleSubmitEvents() {
+    searchButton.click(function() {
+      fetchData();
+    });
+    searchForm.submit(function(event) {
+      fetchData();
+      event.preventDefault();
+    });
+  }
+
+
+
+  function init() {
+    handleSubmitEvents();
+  }
+
+
+
+
+  init();
 });
